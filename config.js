e4446361d5d474d120c397986e663dd1eeecfcbc
RCRamsey <72559630+RCRamsey@users.noreply.github.com>
2022-03-16 17:15:44 -0400

    update descriptions in text boxes for story

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ var config = {
       description:
         'In December 2019 the world was introduced to COVID-19 which rapidly grew into a Global Pandemic during 2020. Schools were left scrambling to reform their in person experience to remote learning. Fall of 2020 school year forced educators into a massive pivot of the learning experience. Unfortunately there was not enough time or finances to make equitable accomodations for all students. This storymap begins on the three most populated counties in KY. ',
       location: {
-        center: [-84.2700, 38.8393],
+        center: [-86.2700, 38.4393],
         zoom: 7,
         pitch: 0,
         bearing: 0,
@@ -54,9 +54,9 @@ var config = {
       hidden: false,
       title: 'Fayette County',
         image: 'images/steinar-engeland-GwVmBgpP-PQ-unsplash.jpg',
-      description: '5.2% of student households within Fayette County did not have access to a computer at home. Translating to approximately 6,860 student households.',
+      description: 'Within Fayette County, 5.2% of student households did not have access to a computer at home. This translates to approximately 6,860 student households without a means to participate in live/remote instruction.',
       location: {
-        center: [-84.49610, 38.03089],
+        center: [-84.29610, 38.03089],
         zoom: 10,
         pitch: 0,
         bearing: 0,
@@ -73,7 +73,7 @@ var config = {
       ],
       onChapterExit: [{
         layer: 'tlkycnty19digital-json-nocomp',
-        opacity: 0,
+        opacity: 1,
         duration: 500
        }],
     },
@@ -85,9 +85,9 @@ var config = {
       hidden: false,
       title: 'Jefferson County',
       //   image: './path/to/image/source.png',
-      description: '7.2% of student households within Jefferson County did not have access to a computer at home. Translating to approximately 22,765 student households.',
+      description: 'At Jefferson County, 7.2% of student households did not have access to a computer at home. Translating to approximately 22,765 student households.',
       location: {
-        center: [-85.74136, 38.22234],
+        center: [-85.38136, 38.22234],
         zoom: 10,
         pitch: 0,
         bearing: 0,
@@ -118,9 +118,9 @@ var config = {
       hidden: false,
       title: 'Kenton County',
       //   image: './path/to/image/source.png',
-      description: '6% of student households within Kenton County did not have access to a computer at home. Translating to approximately 3,922 student households.',
+      description: 'While in Kenton County 6% of student households did not have access to a computer at home. Translating to approximately 3,922 student households.',
       location: {
-        center: [-84.50064, 38.86018],
+        center: [-84.50064, 38.96018],
         zoom: 10,
         pitch: 0,
         bearing: 0,
@@ -138,7 +138,7 @@ var config = {
       onChapterExit: [
         {
           layer: 'tlkycnty19digital-json-nocomp',
-          opacity: 1,
+          opacity: 0,
           duration: 500
         }
       ],
@@ -150,7 +150,7 @@ var config = {
       hidden: false,
       title: 'Fayette County ',
       //   image: './path/to/image/source.png',
-      description: 'Add census bureau tracts here for break down of computer access',
+      description: 'Percent of student households without access to a computer by Census Tract',
       location: {
         center: [-84.49610, 38.03089],
         zoom: 1,
@@ -162,17 +162,17 @@ var config = {
       callback: '',
       onChapterEnter: [
         {
-          layer: 'tlkycnty19digital-json-nocomp',
+          layer: 'nocomputer',
           opacity: 1,
           duration: 5000
          }
       ],
       onChapterExit: [
-      //   {
-      //   layer: 'tlkycnty19digital-json-nocomp',
-      //   opacity: 0,
-      //   duration: 5000
-      //  }
+        {
+        layer: 'nocomputer',
+        opacity: 0,
+        duration: 5000
+       }
       ],
     },
 
@@ -183,7 +183,7 @@ var config = {
       hidden: false,
       title: 'Fayette County ',
       //   image: './path/to/image/source.png',
-      description: 'Percent of student households without internet access whose household income is <20k',
+      description: 'Percent of households with income <20k without internet access.',
       location: {
         center: [-84.49610, 38.03089],
         zoom: 1,
@@ -215,7 +215,7 @@ var config = {
       hidden: false,
       title: 'Fayette County ',
       //   image: './path/to/image/source.png',
-      description: 'Percent of student households without internet access whose household income is 24k-74.9k',
+      description: 'Percent of households with income 24k-74.9k without internet access.',
       location: {
         center: [-84.49610, 38.03089],
         zoom: 1,
@@ -247,7 +247,7 @@ var config = {
       hidden: false,
       title: 'Fayette County ',
       //   image: './path/to/image/source.png',
-      description: 'Percent of student households without internet access whose household income is 75K +',
+      description: 'Percent of households with income > 75K without internet access.',
       location: {
         center: [-84.49610, 38.03089],
         zoom: 1,
